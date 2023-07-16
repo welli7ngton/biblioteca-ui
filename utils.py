a0534d00626f1f4baa9875d58436885b1bf9384d
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-07-16 14:07:57 -0300

    função converte_data_p_str

---
+++ utils.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+def converte_data_p_str(_dia: datetime, _mes: datetime):
+    dia = str(_dia)
+    mes = str(_mes)
+
+    return f"{int(dia):02d}/{int(mes):02d}"
+
+
+dia = datetime.now().day
+mes = datetime.now().month
+
+data = converte_data_p_str(dia, mes)
+print(data)
+
+
+print(datetime.now().microsecond)
