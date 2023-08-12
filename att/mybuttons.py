ec1725b533514d49918b638bee47b6dd7bd5f40a
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-12 16:34:28 -0300

    módulo para botões

---
+++ att/mybuttons.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QPushButton
+
+
+class MyButtons(QPushButton):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        font = self.font()
+        font.setPixelSize(40)
+        font.setBold(True)
+        self.setFont(font)
+        self.setFixedSize(450, 125)
