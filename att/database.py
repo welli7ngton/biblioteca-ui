1fc2738c8175ff9c9f54cc442b34d8f5d5003264
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-16 12:29:54 -0300

    criando função registerLoan para salvar os dados dos empréstimos na base de dados

--- att/database.py
+++ att/database.py
@@ -1,7 +1,12 @@
 import sqlite3
 from student import Student
 from book import Book
-DATABASE_FILE_PATH = 'library.db'
+from datetime import date, timedelta
+
+DATABASE_FILE_PATH = './att/library.db'
+LOAN_PERIOD = 14
+TODAY = date.today()
+DEVOLUTION_DATE = TODAY + timedelta(days=LOAN_PERIOD)
 
 
 class DataBase():
@@ -9,7 +14,7 @@ class DataBase():
         self.connection = sqlite3.connect(DATABASE_FILE_PATH)
         self.cursor = self.connection.cursor()
 
-    def registerStudent(self, student: Student):
+    def registerStudent(self, student: Student) -> None:
 
         self.cursor.execute(
             "INSERT INTO students "
@@ -22,7 +27,7 @@ class DataBase():
         )
         self.connection.commit()
 
-    def registerBook(self, book: Book):
+    def registerBook(self, book: Book) -> None:
 
         self.cursor.execute(
             "INSERT INTO books "
@@ -35,28 +40,56 @@ class DataBase():
         )
         self.connection.commit()
 
-    # TODO: registerLoan
+    def registerLoan(self, student_id: int,
+                     book_id: int, devolution_date=None) -> None:
+
+        if devolution_date:
+            self.cursor.execute(
+                "INSERT INTO loan "
+                "(student_id, book_id, loan_date, devolution_date) "
+                "VALUES("
+                f"'{student_id}', '{book_id}', "
+                f"'{TODAY}', '{TODAY + timedelta(days=devolution_date)}' "
+                ")"
+            )
+            self.connection.commit()
+            return
+
+        self.cursor.execute(
+            "INSERT INTO loan "
+            "(student_id, book_id, loan_date, devolution_date) "
+            "VALUES("
+            f"'{student_id}', '{book_id}', "
+            f"'{TODAY}', '{DEVOLUTION_DATE}' "
+            ")"
+        )
+        self.connection.commit()
+
+    def _closeCon(self) -> None:
+        self.cursor.close()
+        self.connection.close()
+
     # TODO: getStudent|BookID
 
 
 if __name__ == "__main__":
-    s1 = Student()
-    s1.setName = "Qeijo"
-    s1.setAdress = "vila acude distrito federal jaburu 12"
-    s1.setAge = 12
-    s1.setContactNumber = "1232148931238"
-    s1.setGradeYear = "3"
-    s1.setShift = "noturno"
+    # s1 = Student()
+    # s1.setName = "Qeijo"
+    # s1.setAdress = "vila acude distrito federal jaburu 12"
+    # s1.setAge = 12
+    # s1.setContactNumber = "1232148931238"
+    # s1.setGradeYear = "3"
+    # s1.setShift = "noturno"
 
-    b1 = Book()
-    b1.setTitle = "Harry Potter"
-    b1.setAmount = 100
-    b1.setAuthor = "Augusto ferreira"
-    b1.setGender = "fantasia"
-    b1.setPublishingCompany = "sei la"
+    # b1 = Book()
+    # b1.setTitle = "ingles"
+    # b1.setAmount = 200
+    # b1.setAuthor = "escola"
+    # b1.setGender = "educacao"
+    # b1.setPublishingCompany = "governo"
 
     b = DataBase()
-    b.registerStudent(s1)
-    b.registerBook(b1)
-    b.cursor.close()
-    b.connection.close()
+    # b.registerStudent(s1)
+
+    b.registerLoan(99, 6, 5)
+    b._closeCon()
