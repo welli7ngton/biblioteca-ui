f6398a739cf6feb3c8614de95c35fe66e287cb4a
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-17 10:45:45 -0300

    tornando o arquivo genérico para armazenar os layouts de todas as abas do programa

---
+++ att/tab_layouts.py
@@ -0,0 +1,30 @@
+from PySide6.QtWidgets import QVBoxLayout
+from PySide6.QtCore import Qt
+from mybuttons import MyButtons
+
+
+class StudentLayout(QVBoxLayout):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        register = MyButtons("Cadastrar Aluno")
+        changeRegister = MyButtons("Alterar Cadastro")
+        makeRegisteredReport = MyButtons("Relatório de cadastros")
+        deleteRegister = MyButtons("Apagar Cadastro")
+
+        self.addWidget(
+            register,
+            alignment=Qt.AlignmentFlag.AlignCenter
+            )
+        self.addWidget(
+            changeRegister,
+            alignment=Qt.AlignmentFlag.AlignCenter
+            )
+        self.addWidget(
+            makeRegisteredReport,
+            alignment=Qt.AlignmentFlag.AlignCenter
+            )
+        self.addWidget(
+            deleteRegister,
+            alignment=Qt.AlignmentFlag.AlignCenter
+            )
