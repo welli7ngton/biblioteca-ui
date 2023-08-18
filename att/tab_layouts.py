d68a317ade2f9ab76ec93b6fa57d289ae28538d2
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-18 14:44:42 -0300

    criando comunicação entre janelas secundárias e abas do programa

--- att/tab_layouts.py
+++ att/tab_layouts.py
@@ -1,30 +1,59 @@
-from PySide6.QtWidgets import QVBoxLayout
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QGridLayout
 from mybuttons import MyButtons
+from function_windows import StudentRegisterWindow
+from function_windows import BookRegisterWindow
 
 
-class StudentLayout(QVBoxLayout):
+class StudentLayout(QGridLayout):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        register = MyButtons("Cadastrar Aluno")
+        self.registerWindow = StudentRegisterWindow()
+        self.changeRegisterWindow = StudentRegisterWindow(changeRegister=True)
+
+        register = MyButtons("Cadastro - Aluno")
         changeRegister = MyButtons("Alterar Cadastro")
         makeRegisteredReport = MyButtons("Relatório de cadastros")
         deleteRegister = MyButtons("Apagar Cadastro")
 
-        self.addWidget(
-            register,
-            alignment=Qt.AlignmentFlag.AlignCenter
-        )
-        self.addWidget(
-            changeRegister,
-            alignment=Qt.AlignmentFlag.AlignCenter
-        )
-        self.addWidget(
-            makeRegisteredReport,
-            alignment=Qt.AlignmentFlag.AlignCenter
-        )
-        self.addWidget(
-            deleteRegister,
-            alignment=Qt.AlignmentFlag.AlignCenter
-        )
+        self.addWidget(register, 1, 1)
+        self.addWidget(changeRegister, 2, 1)
+        self.addWidget(makeRegisteredReport, 3, 1)
+        self.addWidget(deleteRegister, 4, 1)
+
+        register.clicked.connect(self.registerWindow.show)
+        changeRegister.clicked.connect(self.changeRegisterWindow.show)
+
+
+class BookLayout(QGridLayout):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.registerWindow = BookRegisterWindow()
+        self.changeRegisterWindow = BookRegisterWindow(changeRegister=True)
+
+        register = MyButtons("Cadastro - Livro")
+        changeRegister = MyButtons("Alterar Cadastro")
+        makeRegisteredReport = MyButtons("Relatório de cadastros")
+        deleteRegister = MyButtons("Apagar Cadastro")
+
+        self.addWidget(register, 1, 1)
+        self.addWidget(changeRegister, 2, 1)
+        self.addWidget(makeRegisteredReport, 3, 1)
+        self.addWidget(deleteRegister, 4, 1)
+
+        register.clicked.connect(self.registerWindow.show)
+        changeRegister.clicked.connect(self.changeRegisterWindow.show)
+
+
+class loanAndDevolutionLayout(QGridLayout):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        loan = MyButtons("Realizar Empréstimo")
+        devolution = MyButtons("Realizar Devolução")
+        makeLoanReport = MyButtons("Relatório - Empréstimos")
+
+        self.addWidget(loan, 1, 1)
+        self.addWidget(devolution, 2, 1)
+        self.addWidget(makeLoanReport, 3, 1)
