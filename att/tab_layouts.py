0a5204226c6071e23552708ccd53741d07f861fa
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-19 14:50:14 -0300

    criando tabelas com as informações dos alunos/livros/devoluções

--- att/tab_layouts.py
+++ att/tab_layouts.py
@@ -1,10 +1,40 @@
-from PySide6.QtWidgets import QGridLayout
-from mybuttons import MyButtons
+from PySide6.QtWidgets import (
+    QVBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
+)
 from function_windows import StudentRegisterWindow
 from function_windows import BookRegisterWindow
+from mybuttons import MyButtons
+from database import DataBase
+
+db = DataBase()
+STUDENTS_INFO = db._getTableInfo("students")
+BOOKS_INFO = db._getTableInfo("books")
+LOAN_INFO = db._getTableInfo("loan")
+db._closeConnectionAndCursor()
 
 
-class StudentLayout(QGridLayout):
+def createTable(headerLabels: list[str], infos) -> QTableWidget:
+    table = QTableWidget()
+    table.setColumnCount(len(headerLabels))
+    table.setHorizontalHeaderLabels(headerLabels)
+    table.horizontalHeader().setStretchLastSection(True)
+    table.setEditTriggers(QTableWidget.NoEditTriggers)
+    table.setFixedHeight(500)
+    table.horizontalHeader().setSectionResizeMode(
+        QHeaderView.Stretch
+    )
+    for i in range(0, len(infos)):
+        table.insertRow(i)
+        for j in range(0, len(infos[i])):
+            table.setItem(
+                i,
+                j,
+                QTableWidgetItem(str(infos[i][j]))
+            )
+    return table
+
+
+class StudentLayout(QVBoxLayout):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
@@ -13,47 +43,79 @@ class StudentLayout(QGridLayout):
 
         register = MyButtons("Cadastro - Aluno")
         changeRegister = MyButtons("Alterar Cadastro")
-        makeRegisteredReport = MyButtons("Relatório de cadastros")
         deleteRegister = MyButtons("Apagar Cadastro")
 
-        self.addWidget(register, 1, 1)
-        self.addWidget(changeRegister, 2, 1)
-        self.addWidget(makeRegisteredReport, 3, 1)
-        self.addWidget(deleteRegister, 4, 1)
+        self.studentTable = createTable(
+            [
+                "ID",
+                "NOME",
+                "IDADE",
+                "CONTATO",
+                "ENDEREÇO",
+                "TURNO",
+                "SÉRIE"
+            ],
+            STUDENTS_INFO
+        )
+
+        self.addWidget(self.studentTable)
+        self.addWidget(register)
+        self.addWidget(changeRegister)
+        self.addWidget(deleteRegister)
 
         register.clicked.connect(self.registerWindow.show)
         changeRegister.clicked.connect(self.changeRegisterWindow.show)
 
 
-class BookLayout(QGridLayout):
+class BookLayout(QVBoxLayout):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self.registerWindow = BookRegisterWindow()
         self.changeRegisterWindow = BookRegisterWindow(changeRegister=True)
+        self.bookTable = createTable(
+            [
+                "ID",
+                "TÍTULO",
+                "AUTOR",
+                "EDITORA",
+                "GÊNERO",
+                "QUANTIDADE"
+            ],
+            BOOKS_INFO
+        )
 
+        self.addWidget(self.bookTable)
         register = MyButtons("Cadastro - Livro")
         changeRegister = MyButtons("Alterar Cadastro")
-        makeRegisteredReport = MyButtons("Relatório de cadastros")
         deleteRegister = MyButtons("Apagar Cadastro")
 
-        self.addWidget(register, 1, 1)
-        self.addWidget(changeRegister, 2, 1)
-        self.addWidget(makeRegisteredReport, 3, 1)
-        self.addWidget(deleteRegister, 4, 1)
+        self.addWidget(register)
+        self.addWidget(changeRegister)
+        self.addWidget(deleteRegister)
 
         register.clicked.connect(self.registerWindow.show)
         changeRegister.clicked.connect(self.changeRegisterWindow.show)
 
 
-class loanAndDevolutionLayout(QGridLayout):
+class loanAndDevolutionLayout(QVBoxLayout):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
+        self.loanTable = createTable(
+            [
+                "ID EMPŔESTIMO",
+                "ID ALUNO",
+                "ID LIVRO",
+                "DATA EMPRÉSTIMO",
+                "DATA DEVOLUÇÃO"
+            ],
+            LOAN_INFO
+        )
+
+        self.addWidget(self.loanTable)
         loan = MyButtons("Realizar Empréstimo")
         devolution = MyButtons("Realizar Devolução")
-        makeLoanReport = MyButtons("Relatório - Empréstimos")
 
-        self.addWidget(loan, 1, 1)
-        self.addWidget(devolution, 2, 1)
-        self.addWidget(makeLoanReport, 3, 1)
+        self.addWidget(loan)
+        self.addWidget(devolution)
