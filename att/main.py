42a064cf754f85035230acb79a30dc9b5ee7a9c1
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-21 15:04:50 -0300

    removendo criação de instâncias que não serão usadas no módulo principal

--- att/main.py
+++ att/main.py
@@ -1,15 +1,9 @@
-from student_and_book import Student
-from student_and_book import Book
-from database import DataBase
+
 from PySide6.QtWidgets import QApplication
 from ui import MainWindow
 import sys
 
 if __name__ == "__main__":
-    s1 = Student()
-    b1 = Book()
-    db = DataBase()
-
     app = QApplication(sys.argv)
 
     mainWindow = MainWindow()
